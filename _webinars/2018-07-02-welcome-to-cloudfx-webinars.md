636b388db8dce4ec5b4dad14ffdb4c6714b99b2b
developerbhuwan <bhuwan.upadhyay49@gmail.com>
2018-07-03 08:49:04 +0400

    updates

--- _webinars/2018-07-02-welcome-to-cloudfx-webinars.md
+++ _webinars/2018-07-02-welcome-to-cloudfx-webinars.md
@@ -1,9 +1,9 @@
 ---
-title: Welcome to First Webinar
+title: Welcome To Cloudfx Webinars
 author: developerbhuwan
 category: general
 image:
 image_featured: false
-permalink: /webinars/2018/07/02/welcome-to-first-webinar.html
+permalink: /webinars/welcome-to-cloudfx-webinars.html
 ---
 You guys asked for it, and here it is.
--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 #bundle install
 #bundle exec jekyll build
-bundle exec jekyll serve
+#bundle exec jekyll serve
+
+rm -rf tmp
+rm -rf _site
+current=${PWD}
+bundle exec jekyll build
+mkdir tmp && cd tmp
+repo=cloudfx.github.io
+git clone https://devbhuwan@github.com/cloudfx/${repo}.git
+cd ${repo}
+cp -R ${current}/_site/* .
+git add *
+git commit -m "Updated: $1"
+git push
+cd ${current}
+rm -rf tmp
+rm -rf _site
